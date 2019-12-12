77f3414f54792dbc14d74bd58eaa121ec3f5c41f
Li <lfqt5@mst.edu>
2019-12-12 11:33:09 -0500

    first commit of CS170

---
+++ CS170/makefile/main.cpp
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include "message.h"
+
+using namespace std;
+
+int main()
+{
+	message m;
+	m.printMessage();
+
+	return 0;
+}
+
---
+++ CS170/makefile/message.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "message.h"
+
+using namespace std;
+
+void message::printMessage(){
+		cout << "Second New Makefile example\n";
+}
